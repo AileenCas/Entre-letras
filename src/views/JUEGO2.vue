3a8fdece61e4e9728a83401b4de6a5f0a22e3a4c
AileenCas <53542264+AileenCas@users.noreply.github.com>
2019-10-03 08:15:18 -0500

    Juego2

--- src/views/JUEGO2.vue
+++ src/views/JUEGO2.vue
@@ -1,74 +1,28 @@
 <template>
-    <div id='app'>
-        <ul class="todo-list">
-            <li @dragover.prevent @drop="dragFinish(-1, $event)" v-if="dragging > -1" class="trash-drop todo-item" v-bind:class="{drag: isDragging}">Delete</li>
+    <div class="div1" ondrop="drop()" ondragover="allowDrop()"></div>
+   <img src="../assets/lupita-t.png"  ondragstart="drag()">
 
-            <li v-else>
-                <input placeholder="Type new task and press enter" type="text" class="new-todo todo-item" v-model="newItem" @keyup.enter="addItem">
-            </li>
-            <li class="todo-item" v-for="(item, i) in todos" v-key="i" draggable="true" @dragstart="dragStart(i, $event)" @dragover.prevent @dragenter="dragEnter" @dragleave="dragLeave" @dragend="dragEnd" @drop="dragFinish(i, $event)">
-                <input type="checkbox" v-model="item.done" />
-                <span :class="{done: item.done}">{{ item.title }}</span>
-                <span class="remove-item" @click="removeItem(item)">x</span>
-            </li>
-        </ul>
-    </div>
 </template>
 
 <script>
     export default {
         NAME: 'JUEGO2',
-    }
+        Methods: {
+            allowDrop: function (ev) {
+                ev.preventDefault();
+            },
+            drag: function (ev) {
+                ev.dataTransfer.setData("text", ev.target.id);
+            },
+            drop: function (ev) {
+                ev.preventDefault();
+                var data = ev.dataTransfer.getData("text");
+                ev.target.appendChild(document.getElementById(data));
+            },
+        }}
 
 </script>
 
 <style scoped>
-    * {
-        box-sizing: border-box;
-    }
 
-    .todo-list {
-        list-style-type: none;
-        padding: 10px;
-    }
-
-    .done {
-        text-decoration: line-through;
-        color: #888;
-    }
-
-    .new-todo {
-        width: 100%;
-    }
-
-    .trash-drop {
-        border: 2px dashed #ccc !important;
-        text-align: center;
-        color: #e33;
-    }
-
-    .trash-drop:-moz-drag-over {
-        border: 2px solid red;
-    }
-
-    .todo-item {
-        border: 1px solid #ccc;
-        border-radius: 2px;
-        padding: 14px 8px;
-        margin-bottom: 3px;
-        background-color: #fff;
-        box-shadow: 1px 2px 2px #ccc;
-        font-size: 22px;
-    }
-
-    .remove-item {
-        float: right;
-        color: #a45;
-        opacity: 0.5;
-    }
-
-    .todo-item:hover .remove-item {
-        opacity: 1;
-        font-size: 28px;
-    }
 </style>
