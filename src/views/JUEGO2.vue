18e0c2d67856449ea266407e164c88dd91a50a26
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2019-10-09 09:36:34 -0500

    Agregando Toast a Agudas
    Se agregó Toast a los juegos de palabras agudas
    
    Se corrigieron algunos errores, no aparecia la primera pregunta al terminar el juego
    
    Se actualizaron los paquetes de Node.

--- src/views/JUEGO2.vue
+++ src/views/JUEGO2.vue
@@ -1,8 +1,8 @@
 <template>
-      <div id="app">
-          <div class="div1" ondrop="drop()" ondragover="allowDrop()"></div>
-        <img src="../assets/lupita-t.png"  ondragstart="drag()">
-        </div>
+    <div id="app">
+        <div class="div1" ondrop="drop()" ondragover="allowDrop()"></div>
+        <img src="../assets/lupita-t.png" ondragstart="drag()">
+    </div>
 
 </template>
 
@@ -10,30 +10,32 @@
     export default {
         NAME: 'JUEGO2',
         Methods: {
-                    allowDrop: function (event) {
-                        event.preventDefault();
-                        console.log(event)
-                    },
-                    drag: function (ev) {
-                        ev.dataTransfer.setData("text", ev.target.id);
-                            console.log(event)
-                        },
-                        drop: function (ev) {
-                            ev.preventDefault();
-                            var data = ev.dataTransfer.getData("text");
-                            ev.target.appendChild(document.getElementById(data));
-                                console.log(event)
-                            },
-                        }}
+            allowDrop: function (event) {
+                event.preventDefault();
+                console.log(event)
+            },
+            drag: function (ev) {
+                ev.dataTransfer.setData("text", ev.target.id);
+                console.log(event)
+            },
+            drop: function (ev) {
+                ev.preventDefault();
+                var data = ev.dataTransfer.getData("text");
+                ev.target.appendChild(document.getElementById(data));
+                console.log(event)
+            },
+        }
+    }
 
 </script>
 
 <style scoped>
 
-    .div1{
+    .div1 {
         border: 1px #0a0108 solid;
     }
-    img{
+
+    img {
         width: 100px;
         height: 100px;
     }
