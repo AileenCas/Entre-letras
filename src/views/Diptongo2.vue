a563caea6ca6b836a9dd7383beb2dc15c718a866
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2019-10-17 21:45:39 -0500

    Correcion de Diptongo 2

--- src/views/Diptongo2.vue
+++ src/views/Diptongo2.vue
@@ -53,7 +53,7 @@
             }
         },
         mounted() {
-            firebase.firestore().collection("Diptongo2").doc(this.number.toString(10)).get().then(doc => {
+            firebase.firestore().collection("Diptongo 2").doc(this.number.toString(10)).get().then(doc => {
                 this.p = doc.data().P;
                 this.op1 = doc.data().R1;
                 this.op2 = doc.data().R2;
@@ -69,7 +69,7 @@
                         this.rc = aux.value;
                     }
                 }
-                firebase.firestore().collection("Diptongo2").doc(this.number.toString(10)).get().then(doc => {
+                firebase.firestore().collection("Diptongo 2").doc(this.number.toString(10)).get().then(doc => {
                     if (doc.data().Rc === this.rc) {
                         this.gano();
                         this.componentKey += 1;
@@ -97,7 +97,7 @@
                 this.op3 = '';
                 this.op4 = '';
                 this.p = '';
-                firebase.firestore().collection("Diptongo2").doc(this.number.toString(10)).get().then(doc => {
+                firebase.firestore().collection("Diptongo 2").doc(this.number.toString(10)).get().then(doc => {
                     if (typeof (doc.data()) !== "undefined") {
                         this.p = doc.data().P;
                         this.op1 = doc.data().R1;
@@ -107,7 +107,7 @@
                     } else {
                         this.number=1;
 
-                        firebase.firestore().collection("Diptongo2").doc("1").get().then(doc => {
+                        firebase.firestore().collection("Diptongo 2").doc("1").get().then(doc => {
                             this.p = doc.data().P;
                             this.op1 = doc.data().R1;
                             this.op2 = doc.data().R2;
@@ -142,7 +142,7 @@
                 }else{
                     this.mostrarToast("Terminaste el juego, tu puntaje fue de: " + this.puntaje);
                     this.number=1;
-                    firebase.firestore().collection("Diptongo2").doc("1").get().then(doc => {
+                    firebase.firestore().collection("Diptongo 2").doc("1").get().then(doc => {
                         this.p = doc.data().P;
                         this.op1 = doc.data().R1;
                         this.op2 = doc.data().R2;
