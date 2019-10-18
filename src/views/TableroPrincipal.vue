311166c6d862e28219503ea53e8e4b98faae3e73
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2019-10-17 21:17:44 -0500

    Actualizando Colores y Perfil

--- src/views/TableroPrincipal.vue
+++ src/views/TableroPrincipal.vue
@@ -2,26 +2,28 @@
     <div id="app">
         <Menu/>
         <Toolbar/>
-        <ion-card color="primary" button="true" class="cards" @click="irA('Temas')">
+        <ion-list>
+        <ion-card color="primary" button="true" class="cards cardsTablero" @click="irA('Temas')">
             <ion-card-header>
                 <ion-card-title>¡Amplía tus conocimientos!</ion-card-title>
             </ion-card-header>
         </ion-card>
-        <ion-card  color="primary" button="true" class="cards" @click="irA('retos')">
+        <ion-card  color="primary" button="true" class="cards cardsTablero" @click="irA('retos')">
             <ion-card-header>
                 <ion-card-title>¡Ponte a prueba!</ion-card-title>
             </ion-card-header>
         </ion-card>
-        <ion-card color="primary" button="true" class="cards" @click="irA('Resultados')">
+        <ion-card color="primary" button="true" class="cards cardsTablero" @click="irA('Resultados')">
             <ion-card-header>
                 <ion-card-title>¡Conoce tus resultados!</ion-card-title>
             </ion-card-header>
         </ion-card>
-        <ion-card color="primary" button="true" class="cards" @click="irA('Tips')">
+        <ion-card color="primary" button="true" class="cards cardsTablero" @click="irA('Tips')">
             <ion-card-header>
                 <ion-card-title>Curiosidades</ion-card-title>
             </ion-card-header>
         </ion-card>
+        </ion-list>
     </div>
 </template>
 
@@ -43,11 +45,19 @@
             }
         },
         mounted() {
-            document.body.style.background = "#283593";
         }
     }
 </script>
 
 <style scoped>
     @import "../assets/styles.css";
+    .cardsTablero{
+        height: 120px;
+        display: flex;
+        align-items: center;
+        background: #2196F3;
+    }
+    ion-card-title{
+        font-size: 24px;
+    }
 </style>
