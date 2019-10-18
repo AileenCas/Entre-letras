0c879ebf16ab1ec0abaeb5136c4fa0211f4fdd7a
AileenCas <53542264+AileenCas@users.noreply.github.com>
2019-10-18 12:26:58 -0500

    Puntaje

--- src/views/Resultados.vue
+++ src/views/Resultados.vue
@@ -5,144 +5,25 @@
          <h2>¡Es hora de conocer tus resultados! </h2>
          <img  class="re" src="../assets/icon-1.png" >
          <ion-button expand="full" class="btnD" @click="Re">Te estábamos esperando</ion-button>
+         <ion-item>
+             <ion-label> <ion-icon name="trophy"></ion-icon>Nivel:</ion-label>
+             <ion-note slot="end" color="tertiary"> <ion-input id="nivel" class="input" readonly></ion-input></ion-note>
+         </ion-item>
 
-         <ion-grid>
-            <ion-row>
-                <ion-col>
-                <div> Tema </div>
-                </ion-col>
-                <ion-col>
-                    <div> Puntaje </div>
-                </ion-col>
-            </ion-row>
-             <ion-row>
-                 <ion-col>
-                 <div> Palabras agudas</div>
-             </ion-col>
-                 <ion-col>
-                     <div> Puntaje </div>
-                 </ion-col>
-             </ion-row>
-             <ion-row>
-                 <ion-col>
-                 <div> Palabras graves</div>
-             </ion-col>
-                 <ion-col>
-                     <div> Puntaje </div>
-                 </ion-col>
-             </ion-row>
-             <ion-row>
-                 <ion-col>
-                 <div> Palabras esdrújulas</div>
-             </ion-col>
-                 <ion-col>
-                     <div> Puntaje </div>
-                 </ion-col>
-             </ion-row>
-             <ion-row>
-                 <ion-col>
-                 <div> Palabras sobresdrújulas</div>
-             </ion-col>
-                 <ion-col>
-                     <div> Puntaje </div>
-                 </ion-col>
-             </ion-row>
-             <ion-row>
-                 <ion-col>
-                 <div> Uso del punto</div>
-             </ion-col>
-                 <ion-col>
-                     <div> Puntaje </div>
-                 </ion-col>
-             </ion-row>
-             <ion-row>
-                 <ion-col>
-                 <div>Diptongos</div>
-             </ion-col>
-                 <ion-col>
-                     <div> Puntaje </div>
-                 </ion-col>
-             </ion-row>
-             <ion-row>
-                 <ion-col>
-                 <div> Triptongos</div>
-             </ion-col>
-                 <ion-col>
-                     <div> Puntaje </div>
-                 </ion-col>
-             </ion-row>
-             <ion-row>
-                 <ion-col>
-                 <div> Hiato</div>
-             </ion-col>
-                 <ion-col>
-                     <div> Puntaje </div>
-                 </ion-col>
-             </ion-row>
-             <ion-row>
-                 <ion-col>
-                 <div> Uso de la coma</div>
-             </ion-col>
-                 <ion-col>
-                     <div> Puntaje </div>
-                 </ion-col>
-             </ion-row>
-             <ion-row>
-                 <ion-col>
-                 <div> Uso de la tilde</div>
-             </ion-col>
-                 <ion-col>
-                     <div> Puntaje </div>
-                 </ion-col>
-             </ion-row>
-             <ion-row>
-                 <ion-col>
-                 <div> Tilde diacrítica</div>
-             </ion-col>
-                 <ion-col>
-                     <div> Puntaje </div>
-                 </ion-col>
-             </ion-row>
-                 <ion-row>
-                     <ion-col>
-                     <div> Tilde en palabras compuestas</div>
-                 </ion-col>
-                     <ion-col>
-                         <div> Puntaje </div>
-                     </ion-col>
-                 </ion-row>
-                     <ion-row>
-                         <ion-col>
-                         <div> Uso de las comillas</div>
-                     </ion-col>
-                         <ion-col>
-                             <div> Puntaje </div>
-                         </ion-col>
-                     </ion-row>
-                         <ion-row>
-                             <ion-col>
-                             <div>Signos de exclamación</div>
-                         </ion-col>
-                             <ion-col>
-                                 <div> Puntaje </div>
-                             </ion-col>
-                         </ion-row>
-                             <ion-row>
-                                 <ion-col>
-                                 <div> Signos de interrogación</div>
-                             </ion-col>
-                                 <ion-col>
-                                     <div> Puntaje </div>
-                                 </ion-col>
-             </ion-row>
+         <ion-item>
+             <ion-label><ion-icon name="logo-game-controller-b"></ion-icon>EXP:</ion-label>
+             <ion-note slot="end" color="success"><ion-input id="exp" readonly class="input"></ion-input></ion-note>
+         </ion-item>
 
-         </ion-grid>
-    </div>
+
+     </div>
 
 </template>
 <script>
     import Menu from "./Menu";
     import Toolbar from "./Toolbar";
+    import firebase from '../libFirebase';
+    import db from '../libFirestore';
     export default {
         name: "Resultados",
         components: {Toolbar, Menu},
@@ -154,7 +35,14 @@
                         subHeader:'¡Lo lograste! Es hora de conquistar el mundo.',
                         }).then(a => a.present());
             }
-        }
+        },
+        mounted() {
+            db.collection('usuarios').doc(firebase.auth().currentUser.email).get().then(doc => {
+                document.getElementById('nivel').value = doc.data().nivel;
+                document.getElementById('exp').value = doc.data().exp + ' XP'
+
+            });
+        },
     }
 </script>
 
