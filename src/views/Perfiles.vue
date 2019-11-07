6ec93767e3cc98a0f62db1489a39168b1ce342b1
AileenCas <53542264+AileenCas@users.noreply.github.com>
2019-11-07 12:50:21 -0500

    PERFILES

--- src/views/Perfiles.vue
+++ src/views/Perfiles.vue
@@ -6,11 +6,11 @@
         <img  class="re" src="../assets/icon-1.png" >
         <h4 class="per"> ¡Hola! En este espacio podrás conocer a los creadores de éste mágico e inusual mundo de la ortografía.
             ¡Echa un vistazo, amigo!</h4>
-        <ion-card><img src="../assets/P1.png" class="re">  <ion-item class="i" >Aileen Castro.</ion-item></ion-card>
-        <ion-card><img src="../assets/P2.png" class="re">  <ion-item class="i">Keimmy García.</ion-item></ion-card>
-        <ion-card><img src="../assets/P4.png" class="re">  <ion-item class="i">Kevin Padilla.</ion-item></ion-card>
-        <ion-card><img src="../assets/P2.png" class="re">  <ion-item class="i">Lina Morón.</ion-item></ion-card>
-        <ion-card><img src="../assets/P3.jpeg.jpg" class="re"><ion-item class="i">Valentina García.</ion-item></ion-card>
+        <ion-card><img src="../assets/P1.png" class="re1">  <ion-item class="i" >Aileen Castro.</ion-item></ion-card>
+        <ion-card><img src="../assets/P2.png" class="re1">  <ion-item class="i">Keimmy García.</ion-item></ion-card>
+        <ion-card><img src="../assets/P4.png" class="re1">  <ion-item class="i">Kevin Padilla.</ion-item></ion-card>
+        <ion-card><img src="../assets/P5.png" class="re1">  <ion-item class="i">Lina Morón.</ion-item></ion-card>
+        <ion-card><img src="../assets/P3.jpeg.jpg" class="re1"><ion-item class="i">Valentina García.</ion-item></ion-card>
 
     </div>
 </template>
